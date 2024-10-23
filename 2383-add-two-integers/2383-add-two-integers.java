599854f5accdd61c8bd77efee583d92740813a68
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2024-10-23 22:45:40 +0530

    Time: 0 ms (100%), Space: 40.6 MB (36.95%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.java
+++ 2383-add-two-integers/2383-add-two-integers.java
@@ -1,8 +1,9 @@
 class Solution {
     public int sum(int num1, int num2) {
-        if(-100<=num1 || num2<=100){
-            return num1+num2;
-        }
-        return 0;
+        // if(-100<=num1 || num2<=100){
+        //     return num1+num2;
+        // }
+        // return 0;
+        return num1+num2;
     }
 }
