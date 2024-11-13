5414eb79f76104081be2660fc397d71b76841321
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2024-11-13 20:33:56 +0530

    Time: 0 ms (100%), Space: 40.1 MB (68.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,8 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         if(n<=2) return n;
-        int first=1; 
-        int second=2;
+        int first=1, second=2;
         for(int i=3;i<=n;i++){
             int third=first+second;
             first=second;
