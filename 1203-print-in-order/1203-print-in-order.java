e21f6c5be83f1c619ec3aaa955af7aedf759aed2
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2025-01-11 09:55:09 +0530

    Time: 12 ms (56.75%), Space: 42.2 MB (58.29%) - LeetHub

---
+++ 1203-print-in-order/1203-print-in-order.java
@@ -0,0 +1,26 @@
+class Foo {
+    private final Semaphore  first = new Semaphore(0), second = new Semaphore(0);
+
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        first.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        first.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        second.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        second.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
