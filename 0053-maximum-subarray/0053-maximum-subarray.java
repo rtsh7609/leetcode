387d3874a04ff2ed0e7a8e7e4581da60ba6f33d1
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2025-03-15 22:00:30 +0530

    Time: 1 ms (99.5%), Space: 57.3 MB (31.56%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -12,6 +12,34 @@ class Solution {
             res = Math.max(res, total);
         }
 
-        return res;        
+        return res;       
+        // int cs=0;
+        // int ms=nums[0];
+        // int prefix[]=new int[nums.length];
+        // prefix[0]=nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     prefix[i]=prefix[i-1]+nums[i];
+        // }
+        // for(int i=0;i<nums.length;i++){
+        //     int s=i;
+        //     for(int j=i;j<nums.length;j++){
+        //         int e=j;
+        //         cs=s==0?prefix[e]:prefix[e]-prefix[s-1];
+        //         if(ms<cs){
+        //             ms=cs;
+        //         }
+        //     }
+        // }
+        // return ms;
+    // int cs=0;
+    // int ms=nums[0];
+    // for(int i=0;i<nums.length;i++){
+    //     cs+=nums[i];
+    //     if(cs<0){
+    //         cs=0;
+    //     }
+    //     ms=Math.max(ms,cs);
+    // }
+    // return ms;
     }
 }
