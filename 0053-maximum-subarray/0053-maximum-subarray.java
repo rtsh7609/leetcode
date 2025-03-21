619ea3328753cecb081c888dc82fc976748d599f
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2025-03-21 19:34:49 +0530

    Time: 1 ms (99.54%), Space: 57 MB (69.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -31,6 +31,16 @@ class Solution {
         //     }
         // }
         // return ms;
+    // int cs=0;
+    // int ms=nums[0];
+    // for(int i=0;i<nums.length;i++){
+    //     if(cs<0){
+    //         cs=0;
+    //     }
+    //     cs+=nums[i];
+    //     ms=Math.max(ms,cs);
+    // }
+    // return ms;
     int cs=0;
     int ms=nums[0];
     for(int i=0;i<nums.length;i++){
