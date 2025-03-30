a82ccfb5003800b57da8b92eeee7494ecfe138f1
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2025-03-30 20:49:54 +0530

    Time: 1 ms (99.57%), Space: 56.7 MB (88.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,66 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // int res = nums[0];
-        // int total = 0;
-
-        // for (int n : nums) {
-        //     if (total < 0) {
-        //         total = 0;
-        //     }
-
-        //     total += n;
-        //     res = Math.max(res, total);
-        // }
-
-        // return res;       
-        // int cs=0;
-        // int ms=nums[0];
-        // int prefix[]=new int[nums.length];
-        // prefix[0]=nums[0];
-        // for(int i=1;i<nums.length;i++){
-        //     prefix[i]=prefix[i-1]+nums[i];
-        // }
-        // for(int i=0;i<nums.length;i++){
-        //     int s=i;
-        //     for(int j=i;j<nums.length;j++){
-        //         int e=j;
-        //         cs=s==0?prefix[e]:prefix[e]-prefix[s-1];
-        //         if(ms<cs){
-        //             ms=cs;
-        //         }
-        //     }
-        // }
-        // return ms;
-    // int cs=0;
-    // int ms=nums[0];
-    // for(int i=0;i<nums.length;i++){
-    //     if(cs<0){
-    //         cs=0;
-    //     }
-    //     cs+=nums[i];
-    //     ms=Math.max(ms,cs);
-    // }
-    // return ms;
-    // int cs=0;
-    // int ms=nums[0];
-    // for(int i=0;i<nums.length;i++){
-    //     if(cs<0){
-    //         cs=0;
-    //     }
-    //     cs+=nums[i];
-    //     ms=Math.max(ms,cs);
-    // }
-    // return ms;
-    int cs=0;
-    int ms=nums[0];
-    for(int i=0;i<nums.length;i++){
-        
-        if(cs<0){
-            cs=0;
+        int cs=0;
+        int ms=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(cs<0){
+                cs=0;
+            }
+            cs+=nums[i];
+            ms=Math.max(cs,ms);
         }
-        cs+=nums[i];
-        ms=Math.max(cs,ms);
-    }
-    return ms;
+        return ms;
     }
 }
