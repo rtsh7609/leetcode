22209ae889ff5c0ca93280397192c921e1dca120
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2025-04-05 20:12:50 +0530

    Time: 0 ms (100%), Space: 45.4 MB (6.99%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.java
+++ 0898-transpose-matrix/0898-transpose-matrix.java
@@ -5,8 +5,7 @@ class Solution {
         int mat[][]=new int[m][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                mat[j][i]=matrix[i][j];
-
+               mat[j][i]=matrix[i][j]; 
             }
         }
         return mat;
