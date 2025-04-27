e48377f2252d2b3d2409dd4a593e70dfbb01c9eb
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2025-04-27 21:20:58 +0530

    Time: 0 ms (100%), Space: 44.2 MB (77.92%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -1,10 +1,10 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
-        int n=mat.length;
         int sum=0;
+        int n=mat.length;
         for(int i=0;i<n;i++){
             sum+=mat[i][i];
-            if(i!=n-1-i){
+            if(i!=n-i-1){
                 sum+=mat[i][n-1-i];
             }
         }
