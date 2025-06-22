229ad4a9bc0190e896c934a7fe8ad96099b6613c
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2025-06-22 20:42:55 +0530

    Time: 1 ms (99.9%), Space: 61.3 MB (88.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -5,8 +5,9 @@ class Solution {
         for(int i=0;i<prices.length;i++){
             if(buy<prices[i]){
                 int p=prices[i]-buy;
-                mp=Math.max(p,mp);
-            }else{
+                mp=Math.max(mp,p);
+            }
+            else{
                 buy=prices[i];
             }
         }
