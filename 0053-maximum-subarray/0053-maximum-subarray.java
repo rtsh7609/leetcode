3d9fa417ad4e73c8f569f9ae708aefd9eed8c647
rtsh7609 <128120306+rtsh7609@users.noreply.github.com>
2025-07-10 18:09:15 +0530

    Time: 1 ms (99.55%), Space: 56.8 MB (81.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,9 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int cs=0;
+        int cs=nums[0];
         int ms=nums[0];
-        for(int i=0;i<nums.length;i++){
-            if(cs<0){
-                cs=0;
-            }
-            cs+=nums[i];
+        for(int i=1;i<nums.length;i++){
+            cs=Math.max(nums[i],cs+nums[i]);
             ms=Math.max(ms,cs);
         }
         return ms;
